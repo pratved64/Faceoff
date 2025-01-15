30d56305acee3bd6f3b7f42cc21cb014aa44f449
pratved64 <pratyushved12@gmail.com>
2025-01-15 18:38:58 +0530

    Edited reponse() in Card to fit API Calls

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,15 +1,23 @@
-function Card({path, def, elo})
+import { useState } from 'react'
+
+function Card({path, def, initElo})
 {
 
-    function response() {
-        // Place API calls here
-        alert("Clicked! " + def)
+    const [state, setState] = useState({imagePath: path, elo: initElo})
+
+    function response()
+    {
+        setState((prevState) => {
+            // Place API calls here and return object with updated path and elo
+            console.log("Updated State of " + def)
+            return state
+        })
     }
 
     return (
         <div className="card" onClick={response}>
-            <img src={path} alt={def}/>
-            <p>{elo}</p>
+            <img src={state.imagePath} alt={def}/>
+            <p>{state.elo}</p>
         </div>
     )
 }
