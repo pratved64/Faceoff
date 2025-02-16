a9fbe10e492941cd99ed681c4b87db38e12e4f02
Rishabh Rajeev Palany <rishabh.palany2708@gmail.com>
2025-02-16 12:27:29 +0530

    added api calls

--- src/Components/LDList.jsx
+++ src/Components/LDList.jsx
@@ -1,4 +1,5 @@
 import LDRow from "./LDRow"
+import {useState} from "react";
 
 // elements is an array of element object
 // element contains Rank, Name and Image path of each celebrity
@@ -7,14 +8,21 @@ import LDRow from "./LDRow"
 function LDList() {
     function getElements() {
         // Fetch Data here
-        
+        const [data, setValue] = useState({name:'',elo:'',img:''});
+        const fetchData = () => {
+            fetch("https://faceoff-1.onrender.com/api/leaderboard")
+                .then((res) => res.json())
+                .then((data) => {
+                    setValue1({name:data.name,elo:data.elo,img:data.image_path});
+
         let arr = []
+
         for (let i = 4; i <= 10; i++) {
-            let o = { rank: i, name: "Margot Robbie", img: "#" }
+            let o = { rank: i, name: data.name[i], img: data.img[i]}
             arr.push(o)
         }
         return arr;
-    }
+    })
 
     let elements = getElements();
 
@@ -27,6 +35,6 @@ function LDList() {
     );
 }
 
-export default LDList;
+export default LDList;}}
 
 // TODO: Styling for LDRow
