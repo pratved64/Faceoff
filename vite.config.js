537092db89026dbf59d1390e7118d2c461eba969
pratved64 <pratyushved12@gmail.com>
2025-02-17 19:47:57 +0530

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-  tailwindcss()]
+  tailwindcss()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true,
+  },
 })
