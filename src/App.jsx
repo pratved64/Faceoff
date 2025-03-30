00d166fa1d015eb1b055e25e58451d57b378e3a8
Nikshaan <shettynikshaan@gmail.com>
2025-03-31 04:54:29 +0530

    update: fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,19 @@
-import { useSelector } from "react-redux";
-import Card from "./Components/Card.jsx";
+
 import ParticlesComponent from "./Components/particles.jsx"
 import Titlebar from "./Components/TitleBar.jsx"
 import Ldmain from './ldboard/ldmain.jsx'
-import { motion } from "motion/react";
-import { useState, useEffect } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import Card from "./Components/Card.jsx";
 
 function Home() {
-    const [data1, setValue1] = useState({name:'',elo:'',img:''});
-    const [data2, setValue2] = useState({name:'',elo:'',img:''});
-    const gen = useSelector((store) => store.face);
-    const urls = {
-        w: "https://faceoff-2.onrender.com/api/random",
-        m: "https://faceoff-2.onrender.com/api/random_m"
-    }
-    const fetchURL = gen === "w" ? urls.w : urls.m
-    const fetchData = () => {
-        fetch(fetchURL)
-            .then((res) => res.json())
-            .then((data) => {
-                setValue1({name:data.data1,elo:data.data1_elo,img:data.data1_img});
-                setValue2({name:data.data2,elo:data.data2_elo,img:data.data2_img});})
-            .catch(error => console.error(error))
-        }
-
-    useEffect(() => {
-        fetchData();
-    }, [gen]);
-    window.onbeforeunload = function () {
-        window.scrollTo(0, 0);
-      }
-      
     return (
         <div className="min-h-[100svh] flex flex-col pb-4">
             <div className="-z-20"><ParticlesComponent /></div>
             <div className="mt-2">
                 <Titlebar />
+                <Card />
             </div>
-            <motion.div
-             initial = {{ y: "100vh"}}
-             animate = {{y: 0}}
-             transition={{duration: 1.5}}
-             className="flex flex-col lg:flex-row justify-center items-center bg-transparent lg:min-h-[60svh] w-[80%] sm:w-[50%] lg:w-[80%] lg:mt-20 m-auto mt-4 gap-6 lg:gap-12"> 
-                <Card path = {data1.img} def={data1.name} elo={data1.elo} />
-                <Card path = {data2.img} def={data2.name} elo={data2.elo} />
-            </motion.div>
+            
         </div>
     );
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,9 @@
 import PropTypes from "prop-types";
 import { motion } from "motion/react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
-function Card({path, def })
+function Card()
 {
     Card.propTypes = {
         path: PropTypes.any,
@@ -9,31 +11,73 @@ function Card({path, def })
         elo: PropTypes.any,
     };
 
-    function response() {
+    const [name, setName] = useState("Megan Fox");
+    const [flag, setFlag] = useState(true);
+    const [data1, setValue1] = useState({name:'',elo:'',img:''});
+    const [data2, setValue2] = useState({name:'',elo:'',img:''});
+    const gen = useSelector((store) => store.face);
+
+    const urls = {
+        w: "https://faceoff-2.onrender.com/api/random",
+        m: "https://faceoff-2.onrender.com/api/random_m"
+    }
+
+    const fetchURL = gen === "w" ? urls.w : urls.m;
+
+    const fetchData = () => {
+        fetch(fetchURL)
+            .then((res) => res.json())
+            .then((data) => {
+                setValue1({name:data.data1,elo:data.data1_elo,img:data.data1_img});
+                setValue2({name:data.data2,elo:data.data2_elo,img:data.data2_img});})
+            .catch(error => console.error(error))
+        };
+
+    useEffect(() => {
+        fetchData();
+        response(name);
+    }, [flag, gen]);
+
+    function response(name) {
         // Place API calls here
         //alert("Clicked! " + def)
-        const s_name=def;
+        const s_name = name;
+        try{
         fetch("https://faceoff-2.onrender.com/api/rating", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ s_name }),
         })
             .then((res) => res.json())
-            .then((data) => console.log("Response:", data));
-
-
-        window.location.reload();
-    }
+        }
+        catch(err){
+            console.log("error : ", err)
+            }
+        }
 
     return (
-        <div className="flex flex-col justify-center items-center h-full lg:hover:scale-105 bg-black text-white border-2 p-4 shadow-md hover:shadow-lg duration-500 cursor-pointer shadow-red-400" onClick={response}>
+        <motion.div
+             initial = {{ y: "100vh"}}
+             animate = {{y: 0}}
+             transition={{duration: 1.5}}
+             className="flex flex-col lg:flex-row justify-center items-center bg-transparent lg:min-h-[60svh] w-[80%] sm:w-[50%] lg:w-[80%] lg:mt-20 m-auto mt-4 gap-6 lg:gap-12"> 
+        <div className="flex flex-col justify-center items-center h-full lg:hover:scale-105 bg-black text-white border-2 p-4 shadow-md hover:shadow-lg duration-500 cursor-pointer shadow-red-400" onClick={() => {setFlag(!flag), setName(data1.name)}}>
             <motion.img
              initial = {{ opacity: 0}}
              animate = {{opacity: 1}}
              transition={{duration: 0.2, delay: 1.9}}
-             src={path} alt={def} className="h-80 m-2 border-2"/>
-            <p className="text-lg font-semibold font-serif">{def}</p>
+             src={data1.img} alt={data1.name} className="h-80 m-2 border-2"/>
+            <p className="text-lg font-semibold font-serif">{data1.name}</p>
         </div>
+         <div className="flex flex-col justify-center items-center h-full lg:hover:scale-105 bg-black text-white border-2 p-4 shadow-md hover:shadow-lg duration-500 cursor-pointer shadow-red-400" onClick={() => {setFlag(!flag), setName(data2.name)}}>
+            <motion.img
+            initial = {{ opacity: 0}}
+            animate = {{opacity: 1}}
+            transition={{duration: 0.2, delay: 1.9}}
+            src={data2.img} alt={data2.name} className="h-80 m-2 border-2"/>
+            <p className="text-lg font-semibold font-serif">{data2.name}</p>
+        </div>
+        </motion.div>
     )
 }
 
