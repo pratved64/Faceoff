5d04125a9026fca2b58f32669e13504d624a0869
Nikshaan <shettynikshaan@gmail.com>
2025-04-08 22:00:52 +0530

    update: scale animation

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -34,9 +34,9 @@ function Card({path, def })
 
     return (
         <motion.div
-        initial = {{ rotate: "0deg" }}
-        animate = {{ rotate: "360deg" }}
-        transition={{duration: 1.2 }}
+        initial = {{ rotate: "0deg", scale: 0 }}
+        animate = {{ rotate: "360deg", scale: 1 }}
+        transition={{duration: 1.5 }}
         className="flex flex-col justify-center items-center h-full lg:hover:scale-105 bg-black text-white border-2 p-4 shadow-md hover:shadow-lg duration-500 cursor-pointer shadow-red-400" onClick={response}>
             <motion.img
              initial = {{ opacity: 0}}
