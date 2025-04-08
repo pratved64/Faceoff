1a084f7a5a7d91352d52214613276ea6b8de8506
Nikshaan <shettynikshaan@gmail.com>
2025-04-08 19:39:34 +0530

    update: rotate cards

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -33,14 +33,18 @@ function Card({path, def })
         }
 
     return (
-        <div className="flex flex-col justify-center items-center h-full lg:hover:scale-105 bg-black text-white border-2 p-4 shadow-md hover:shadow-lg duration-500 cursor-pointer shadow-red-400" onClick={response}>
+        <motion.div
+        initial = {{ rotate: "0deg" }}
+        animate = {{ rotate: "360deg" }}
+        transition={{duration: 1.2 }}
+        className="flex flex-col justify-center items-center h-full lg:hover:scale-105 bg-black text-white border-2 p-4 shadow-md hover:shadow-lg duration-500 cursor-pointer shadow-red-400" onClick={response}>
             <motion.img
              initial = {{ opacity: 0}}
              animate = {{opacity: 1}}
              transition={{duration: 0.2, delay: 1.9}}
              src={path} alt={def} className="h-80 m-2 border-2"/>
              <p className="text-lg font-semibold font-serif">{def}</p>
-        </div>
+        </motion.div>
     )
 }
 
